618cb7f6c979acf441dad03c5895b58e299aee6f
orthomas <thomas.orjubin@ynov.com>
2025-03-03 11:05:50 +0100

    initiated tests about print

---
+++ Handler.java
@@ -0,0 +1,5 @@
+ public class Handler {
+    public static void main(String[] args) {
+        UIAnimatueur.affiche();
+    }
+ }
---
+++ UIAnimatueur.java
@@ -0,0 +1,17 @@
+public class UIAnimatueur {
+    static String[] ui = {
+    // └ ┴ ┬ ├ ─ ┼ ╜ ╝ ╣ ↕ ╢ ╣ V  ║ ╕ ╖ ╢ ╡ ┤ │ ¼ » ┐ ╛ └ ┴ ┬ ├ ─ ╗ ╝ ╞ ╟   ╛ ┐ └ ┴ ┬ ├ ─ ┼ ╞ ╟ ╚ ╔ ╔ ╩ ╦ ╠ ═ ╬ ╧ ╨ ╤ ╨ ╥ ╙ ╘ ╒ ╠ ╫
+    "   e   ",
+    "   ╧   ",
+    "",
+    "",
+    "",
+    "",
+    };
+
+    public static void affiche() {
+        for (int i=0; i<6; i++) {
+            System.out.println(ui[i]);
+        }
+    }
+}
