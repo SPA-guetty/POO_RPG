c3309053dff6ef6a4663469614c8b82eaae79750
ORTHOMAS <thomas.orjubin@ynov.com>
2025-03-10 16:03:11 +0100

    almost finished players graphics

--- UIAnimatueur.java
+++ UIAnimatueur.java
@@ -1,16 +1,38 @@
 public class UIAnimatueur {
     static String[] ui = {
     // └ ┴ ┬ ├ ─ ┼ ╜ ╝ ╣ ↕ ╢ ╣ V  ║ ╕ ╖ ╢ ╡ ┤ │ ¼ » ┐ ╛ └ ┴ ┬ ├ ─ ╗ ╝ ╞ ╟   ╛ ┐ └ ┴ ┬ ├ ─ ┼ ╞ ╟ ╚ ╔ ╔ ╩ ╦ ╠ ═ ╬ ╧ ╨ ╤ ╨ ╥ ╙ ╘ ╒ ╠ ╫ ╓╒ ╘ ╢
+    "\nAnimatueur",
     "   の  ",
-    "   漢مــ ",
-    "   克",
-    "",
-    "",
-    "",
+    "   杀مــ ",
+    "   刀",
+    "\nNainformaticien",
+    "   ",
+    "   の",
+    "   技ロ",
+    "\nCyber-serker",
+    "   尊",
+    "  ⛨裁مــ",
+    "   清",
+    "\nCaptchattaque ",
+    "   樂",
+    " Ɵ═攻═Ɵ",
+    "   勇",
+    "\nPère Créa",
+    "   护 ☥",
+    "   息─┤",
+    "   大 │",
+    "\nwebscammer ",
+    "   の",
+    "   播",
+    "   扰",
+    "\nArchicube ",
+    "   护",
+    "   城",
+    "   防",
     };
 
     public static void affiche() {
-        for (int i=0; i<6; i++) {
+        for (int i=0; i<ui.length; i++) {
             System.out.println(ui[i]);
         }
     }
