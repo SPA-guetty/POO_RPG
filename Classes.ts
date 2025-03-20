f24ebd083284b3b7d12a3472c0e698e109819f80
ORTHOMAS <thomas.orjubin@ynov.com>
2025-03-20 08:16:37 +0100

    first menu

---
+++ Classes.ts
@@ -0,0 +1,76 @@
+class Animatueur {
+    UI = [
+        "  の   ",
+        "  杀مــ",
+        "  刀   ",
+    ]
+
+    Processeurs = 8;
+    Défense = 5;
+    Batterie = 80;
+    RAM = 32;
+}
+
+class Nainformaticien {
+    UI = [
+        "       ",
+        "  の   ",
+        "  技ロ ",
+    ]
+
+    Processeurs = 8;
+    Défense = 10;
+    Batterie = 80;
+    RAM = 16;
+
+}
+
+class Cyberserker {
+    UI = [
+        "  尊   ",
+        " ⛨裁مــ",
+        "  清   ",
+    ]
+
+    Processeurs = 8;
+    Défense = 8;
+    Batterie = 100;
+    RAM = 16;
+}
+
+class Captchattaque {
+    UI = [
+        "  樂   ",
+        "Ɵ═攻═Ɵ ",
+        "  勇   ",
+    ]
+
+    Processeurs = 10;
+    Défense = 7;
+    Batterie = 80;
+    RAM = 8;
+}
+
+class Pèrecrea {
+    UI = [
+        "  护 ☥ ",
+        "  息─┤ ",
+        "  大 │ ",
+    ]
+}
+
+class Webscammer {
+    UI = [
+        "  の   ",
+        "  播   ",
+        "  扰   ",
+    ]
+}
+
+class Archicube {
+    UI = [
+        "  护 ⚘ ",
+        " 𓆰城─┤ ",
+        "  防 │ ",
+    ]
+}
